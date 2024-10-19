59a4163f5420ea07c34e40afcedcffa61eb63f57
Natapat-singhara <125733344+Natapat-singhara@users.noreply.github.com>
2024-10-19 17:03:20 +0700

    End Ep2.1

--- python+sqlite_class/GUI-Expense.py
+++ python+sqlite_class/GUI-Expense.py
@@ -21,8 +21,33 @@ def insert_expense(title,price,others):
         c.execute(command,(None,title,price,others,ts))
     conn.commit() # save to database
     print('saved')
+    
+def view_expense():
+    with conn:
+        command = 'SELECT * FROM expense'
+        c.execute(command)
+        result = c.fetchall()
+    return result
 
-###############################
+def update_expense(ID,field,newvalue):
+    with conn:
+        command = 'UPDATE expense SET {} = (?) WHERE ID = (?)'.format(field)
+        c.execute(command,(newvalue,ID))
+    conn.commit()
+
+def update_table():
+    table.delete(*table.get_children()) #Function Clear data in table.
+    for row in view_expense():
+        table.insert('','end',values=row)
+
+def delete_expense(ID):
+    with conn:
+        command = 'DELETE FROM expense WHERE ID = (?)'
+        c.execute(command,([ID])) #ถ้าเงื่อนไขมีตัวเดียวต้องใส่ [] เพิ่ม.เฉพาะ sqllite\
+    conn.commit()
+
+
+############ END SQL ###################
 
 GUI = Tk()
 GUI.title('โปรแกรมบันทึกค่าใช่จ่าย by ลุง')
@@ -32,6 +57,10 @@ FONT1 = ('Angsana New',20)
 
 ######TAB######
 PATH = os.getcwd() # check current folder
+
+mainincon = os.path.join(PATH,'img\Sheep-CD.ico')
+GUI.iconbitmap(mainincon)
+
 Tab = ttk.Notebook(GUI)
 Tab.pack(fill=BOTH,expand=1)
 
@@ -55,7 +84,7 @@ iconimage = PhotoImage(file=icon)
 L = Label(T1,image=iconimage)
 L.pack()
 
-L = Label(GUI,text='โปรแกรมบันทึกค่าใช่จ่าย',font=('Angsana New',30))
+L = Label(T1,text='โปรแกรมบันทึกค่าใช่จ่าย',font=('Angsana New',30))
 L.pack(pady=10) #ทำให้ข้อความติดอยู่ตรงกลาง เรียงจากข้างบนลงล่าง
 
 L = Label(T1,text='รายการ', font=FONT1)
@@ -94,11 +123,13 @@ def Save(event=None):
         V_price.set('')
         V_others.set('')
         E1.focus()
+        update_table()
+        
         # messagebox.showinfo('Message',title)
 
 E3.bind('<Return>',Save) #ใส่ event=Note ในฟังก์ชั่น
 
-B1 = ttk.Button(GUI,text='SAVE',command=Save)
+B1 = ttk.Button(T1,text='SAVE',command=Save)
 B1.pack(ipadx=20,ipady=10)
 
 ###############TAB2################
@@ -115,4 +146,91 @@ for h,w in zip(header,hwidth):
     
 # table.insert('','end',values=[1,'น้ำดื่ม',10,'ร้านป้าแดง','2024-10-19 15:15:10']) #ตัวอย่างของ Record
 
+
+############### DELETE ################
+
+def delete_table(event=None):
+    try:
+        print('delete..')
+        select = table.selection()
+        ID = table.item(select)['values'][0]
+        choice = messagebox.askyesno('ลบข้อมูล','คุณต้องการลบข้อมูลใช่หรือไม่?')
+        # print(choice) #Debug check
+        if choice == True:
+            delete_expense(ID)
+            update_table()
+    except Exception as e:
+            print(e)
+            messagebox.showwarning('แจ้งเตือน','กรุณาเลือกรายการที่ต้องการลบ')
+
+table.bind('<Delete>',delete_table)
+
+############### UPDATE ################
+
+def update_data(event=None):
+    try:
+
+        select = table.selection()
+        data = table.item(select)['values']
+        print(data)
+        
+        GUI2 = Toplevel()
+        GUI2.title('แก้ไขและบันทึกข้อมูล')
+        GUI2.geometry('700x600')
+        
+        L = Label(GUI2,image=iconimage)
+        L.pack()
+
+        L = Label(GUI2,text='หน้าแก้ไขข้อมูล',font=('Angsana New',30))
+        L.pack(pady=10) #ทำให้ข้อความติดอยู่ตรงกลาง เรียงจากข้างบนลงล่าง
+
+        L = Label(GUI2,text='รายการ', font=FONT1)
+        L.pack()
+
+        V_title_update = StringVar()
+        V_title_update.set(data[1])
+        E1 = ttk.Entry(GUI2,textvariable=V_title_update ,font=FONT1, width=50)
+        E1.pack()
+        ###############################
+        L = Label(GUI2,text='ราคา', font=FONT1)
+        L.pack()
+        V_price_update= StringVar()
+        V_price_update.set(data[2])
+        E2 = ttk.Entry(GUI2,textvariable=V_price_update ,font=FONT1, width=50)
+        E2.pack()
+        ###############################
+        L = Label(GUI2,text='หมายเหตุ', font=FONT1)
+        L.pack()
+        V_others_update = StringVar()
+        V_others_update.set(data[3])
+        E3 = ttk.Entry(GUI2,textvariable=V_others_update ,font=FONT1, width=50)
+        E3.pack()
+        
+        def Edit_data():
+            ID = data[0]
+            title_update = V_title_update.get()
+            price_update = float(V_price_update.get())
+            others_update = V_others_update.get()
+            update_expense(ID,'title',title_update)
+            update_expense(ID,'price',price_update)
+            update_expense(ID,'others',others_update)
+            update_table() #อัพเดทข้อมูลใหม่ใน Table เสมอ
+            # print(title_update , price_update , others_update)
+            GUI2.destroy()
+            #ติด ยังแก้ไม่ได้ 5555
+            
+        
+        B1 = ttk.Button(GUI2,text='SAVE',command=Edit_data)
+        B1.pack(ipadx=20,ipady=10)
+        # messagebox.showinfo('แจ้งเตือน','สำเร็จ')
+        GUI2.mainloop
+
+    except Exception as e:
+            print(e)
+            messagebox.showwarning('เลือกรายการ','กรุณาเลือกรายการที่ต้องการอัพเดท')
+
+
+table.bind('<Double-1>',update_data)
+
+update_table()
 GUI.mainloop()
